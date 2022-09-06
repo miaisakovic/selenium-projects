77979535ee21158027625227f2ca7f6ce9037530
Mia Isakovic <47527694+miaisakovic@users.noreply.github.com>
2022-09-06 00:27:46 -0400

    Use Flake8

--- uw_math_news.py
+++ uw_math_news.py
@@ -28,7 +28,8 @@ driver.get("https://mathnews.uwaterloo.ca/")
 
 # Click the link of the most recent Math NEWS article
 try:
-    wait.until(ExpectedCondition.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "mathNEWS ")))
+    wait.until(ExpectedCondition.element_to_be_clickable((By.PARTIAL_LINK_TEXT,
+                                                          "mathNEWS ")))
     article_link = driver.find_elements(By.PARTIAL_LINK_TEXT, "mathNEWS ")
     text = article_link[0].text
     article_link[0].click()
@@ -36,12 +37,13 @@ except Exception:
     assert False, "Can't click the link of the most recent Math NEWS article"
 
 # Override SSL certificate validation
-ssl._create_default_https_context = ssl._create_unverified_context 
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Retrieve the URL of the article
 article_url = driver.current_url
 
-# The relative path to a folder where the article should be saved (does not need to exist) 
+# The relative path to a folder where the article should be saved
+# This folder does not need to exist
 local_folder = os.getenv('LOCAL_FOLDER_FOR_ARTICLE')
 
 if local_folder[-1] != "/":
@@ -51,10 +53,11 @@ if local_folder[-1] != "/":
 if not os.path.isdir(local_folder):
     os.makedirs(local_folder)
 
-# The file name of the saved article 
+# The file name of the saved article
 local_file = text.replace(" ", "_") + "_copy.pdf"
 
-# Download the article and save it in the local folder under the specified file name
+# Download the article and save it in the local folder
+#   under the specified file name
 request.urlretrieve(article_url, local_folder + local_file)
 
 driver.quit()
